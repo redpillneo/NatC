6bf5b7cc7e7c6f8081d0e61e4b9b2f340ffebff2
redpillneo <eulinnathaniel@gmail.com>
2024-01-16 19:20:59 +0800

    try one line in main

--- src/main.c
+++ src/main.c
@@ -3,15 +3,6 @@
 
 int main(int agrc, char* argv[]){
   lexer_T* lexer = init_lexer(
-    "int num1, num2;"
-    "int sum_of_nums;"
-    "str prompt, result;"
-    "prompt = \"Value of num1 and num2: \";"
-    "result = \"Sum of the two numbers: \";"
-    "num1 = 3;"
-    "num2 = 5;"
-    "sum_of_nums = num1 + num2;"
-    "print(prompt, num1, num2);"
     "print(result, sum_of_nums);"
   );
 
